62a6b1e1834de80442bcbd05880d446d1276a2b6
Pjh01 <estelle0102@naver.com>
2025-06-26 08:56:04 +0900

    Add: project file

---
+++ SamsungStoreApp/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SamsungStoreApp
+//
+//  Created by estelle on 6/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+      view.backgroundColor = .systemBackground
+    }
+
+
+}
+
