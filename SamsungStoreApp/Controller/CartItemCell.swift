0a63c6aa3b7447d5eaa2e75a88bfeea3f8c4f970
Gwangyong <key4168@icloud.com>
2025-06-27 00:22:21 +0900

    feat: 장바구니 셀 기본 UI 구현
    - 셀 style을 none로 설정해서 눌림 효과 제거
    - 제품명, 수량 조절 버튼, 수량 표시 라벨, 가격 라벨 추가
    - 버튼 크기 44x44 고정
    - UILabel, UIButton에 대한 configure 메서드 추가

--- SamsungStoreApp/Controller/CartItemCell.swift
+++ SamsungStoreApp/Controller/CartItemCell.swift
@@ -10,28 +10,110 @@ import SnapKit
 import UIKit
 
 final class CartItemCell: UITableViewCell {
-    private let itemLabel = UILabel()
+  private let containerView = UIView()
+  private let stackView = UIStackView()
+  
+  private let itemLabel = UILabel()
+  private let minusButton = UIButton()
+  private let countLabel = UILabel()
+  private let plusButton = UIButton()
+  private let priceLabel = UILabel()
+  
+  private var count: Int = 1
 
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupUI()
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    selectionStyle = .none // cell 누를때 효과 안나오도록
+    setupUI()
+    setupLayout()
+  }
+
+  @available(*, unavailable)
+  required init?(coder _: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: setup
+  private func setupUI() {
+    contentView.addSubview(containerView)
+    containerView.backgroundColor = .white
+    containerView.clipsToBounds = true
+    
+    configureStackView()
+    configureComponents()
+  }
+  
+  // MARK: stackView
+  private func configureStackView() {
+    containerView.addSubview(stackView)
+    stackView.axis = .horizontal
+    stackView.spacing = 12
+    stackView.distribution = .fill
+    stackView.alignment = .center
+    
+    [itemLabel, minusButton, countLabel, plusButton, priceLabel].forEach {
+      stackView.addArrangedSubview($0)
     }
+  }
+  
+  // MARK: components
+  private func configureComponents() {
+    itemLabel.configure(text: "제품명")
+    countLabel.configure(text: "1")
+    priceLabel.configure(text: "10,000원")
 
-    @available(*, unavailable)
-    required init?(coder _: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    minusButton.configure(title: "−")
+    plusButton.configure(title: "+")
+    
+    itemLabel.textAlignment = .left
+    priceLabel.textAlignment = .right
+    
+//    minusButton.setContentHuggingPriority(.required, for: .horizontal)
+//    minusButton.setContentCompressionResistancePriority(.required, for: .horizontal)
+//    plusButton.setContentHuggingPriority(.required, for: .horizontal)
+//    plusButton.setContentCompressionResistancePriority(.required, for: .horizontal)
+//    
+//    countLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+//    countLabel.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+//    
+//    itemLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+//    itemLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+//    
+//    priceLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+//    priceLabel.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+  }
+
+  private func setupLayout() {
+    containerView.snp.makeConstraints {
+      $0.top.bottom.equalToSuperview()
+      $0.leading.trailing.equalToSuperview().inset(24)
     }
-
-    private func setupUI() {
-        contentView.addSubview(itemLabel) // UIKit이 자동생성한 contentView
-
-        itemLabel.text = "제품명 테스트"
-        itemLabel.textColor = .black
-
-        itemLabel.snp.makeConstraints {
-            $0.leading.equalToSuperview().inset(24)
-            $0.centerY.equalToSuperview()
-            $0.height.equalTo(44)
-        }
+    
+    stackView.snp.makeConstraints {
+      $0.directionalEdges.equalToSuperview()
     }
+    
+    minusButton.snp.makeConstraints {
+      $0.width.height.equalTo(44)
+    }
+    
+    plusButton.snp.makeConstraints {
+      $0.width.height.equalTo(44)
+    }
+  }
+
+}
+
+// MARK: - UILabel & UIButton 설정 Extension
+private extension UILabel {
+  func configure(text: String) {
+    self.text = text
+  }
+}
+
+private extension UIButton {
+  func configure(title: String) {
+    self.setTitle(title, for: .normal)
+    self.setTitleColor(.systemBlue, for: .normal)
+  }
 }
