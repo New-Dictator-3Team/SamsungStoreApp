e3402e1cf5e5a84418fb273a6872264592e062ed
Wooseong <thehousedone@gmail.com>
2025-06-26 18:03:51 +0900

    chore: 브랜치 분기 이니셜 커밋입니다.

---
+++ SamsungStoreApp/MenuCollectionView/MenuCollectionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MenuCollectionViewController.swift
+//  SamsungStoreApp
+//
+//  Created by 김우성 on 6/26/25.
+//
+
+import UIKit
+import SnapKit
+
+class MenuCollectionViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    print("view 잘 로드됨")
+    view.backgroundColor = .systemBackground
+    
+  }
+  
+  
+}
