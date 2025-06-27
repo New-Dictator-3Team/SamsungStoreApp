cf8504751fe55ca23579a932f245272927b2fa9b
Wooseong <thehousedone@gmail.com>
2025-06-28 00:24:49 +0900

    chore: 아주 간단한 수정

--- SamsungStoreApp/json/JsonStruct.swift
+++ SamsungStoreApp/json/JsonStruct.swift
@@ -1,29 +1,22 @@
-//
-//  JsonStruct.swift
-//  SamsungStoreApp
-//
-//  Created by 김우성 on 6/27/25.
-//
-
 import Foundation
 
- // MARK: - ProductItem
+// MARK: - ProductItem
 
- struct ProductItem: Decodable {
-   let image: String
-   let name: String
-   let price: String
- }
+struct ProductItem: Decodable {
+  let image: String
+  let name: String
+  let price: String
+}
 
- // MARK: - Category
+// MARK: - Category
 
- struct Category: Decodable {
-   let category: String
-   let items: [ProductItem]
- }
+struct Category: Decodable {
+  let category: String
+  let items: [ProductItem]
+}
 
- // MARK: - CategoryResponse
+// MARK: - CategoryResponse
 
- struct CategoryResponse: Decodable {
-   let categories: [Category]
- }
+struct CategoryResponse: Decodable {
+  let categories: [Category]
+}
