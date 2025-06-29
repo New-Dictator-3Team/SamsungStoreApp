7d9a23080980b2b49310ecf082c89814712a791f
Luca Park <qlife1146@gmail.com>
2025-06-29 17:45:31 +0900

    feat: ViewController에 컴포넌트 추가 style: GetJson에 이모지 제거 style: BottomView swiftFormat 포매팅 적용

--- SamsungStoreApp/View/BottomView.swift
+++ SamsungStoreApp/View/BottomView.swift
@@ -7,14 +7,13 @@
 
 import UIKit
 
-// 하단 결제/취소 버튼 뷰
+/// 하단 결제/취소 버튼 뷰
 class BottomView: UIView {
-  
   // 버튼 생성
-  private let clearButton = BottomButton(title: "전체 취소", type: .clear, fontSize: 18)
-  private let payButton = BottomButton(title: "결제하기", type: .pay, fontSize: 18)
-  
-  // 버튼들을 수평 정렬할 스택뷰
+  private let clearButton: BottomButton = .init(title: "전체 취소", type: .clear, fontSize: 18)
+  private let payButton: BottomButton = .init(title: "결제하기", type: .pay, fontSize: 18)
+
+  /// 버튼들을 수평 정렬할 스택뷰
   private lazy var bottomButtonStack: UIStackView = {
     let stackView = UIStackView(arrangedSubviews: [clearButton, payButton])
     stackView.axis = .horizontal
@@ -23,21 +22,23 @@ class BottomView: UIView {
     stackView.distribution = .fillEqually
     return stackView
   }()
-  
+
   // MARK: - Initializer
+
   override init(frame: CGRect) {
     super.init(frame: frame)
     setupUI()
   }
-  
-  required init?(coder: NSCoder) {
+
+  @available(*, unavailable)
+  required init?(coder _: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-  
+
   // MARK: - UI 구성
+
   private func setupUI() {
     addSubview(bottomButtonStack)
-    
     bottomButtonStack.snp.makeConstraints {
       $0.leading.trailing.equalToSuperview().inset(24)
       $0.top.bottom.equalToSuperview()
